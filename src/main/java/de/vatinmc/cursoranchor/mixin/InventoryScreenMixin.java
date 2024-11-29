40dd47bd0d2ce5790ff22418e1350cb534f6a628
vatinMC <mc@vatin.de>
2024-11-29 17:53:01 +0100

    changed minecraft_version to 1.21.3

--- src/main/java/de/vatinmc/cursoranchor/mixin/InventoryScreenMixin.java
+++ src/main/java/de/vatinmc/cursoranchor/mixin/InventoryScreenMixin.java
@@ -2,13 +2,9 @@ package de.vatinmc.cursoranchor.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.gui.screen.recipebook.RecipeBookProvider;
 import net.minecraft.client.util.Window;
-import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.screen.PlayerScreenHandler;
-import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -18,14 +14,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import oshi.util.tuples.Pair;
 
 @Mixin(InventoryScreen.class)
-public abstract class InventoryScreenMixin  extends AbstractInventoryScreen<PlayerScreenHandler> implements RecipeBookProvider{
+public abstract class InventoryScreenMixin implements RecipeBookProvider{
     @Unique
     private static Pair<Integer, Integer> lastPos = new Pair<>(0,0);
     @Unique
     private boolean firstFrame = true;
-    public InventoryScreenMixin(PlayerScreenHandler screenHandler, PlayerInventory playerInventory, Text text) {
-        super(screenHandler, playerInventory, text);
-    }
 
     @Inject(method = "render", at = @At("TAIL"))
     private void render(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo info){
