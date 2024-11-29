e72eff96f46264bc318358ac319cb36b40bf7744
vatinMC <mc@vatin.de>
2024-11-29 15:11:01 +0100

    setting up

---
+++ src/main/java/de/vatinmc/cursoranchor/client/CursorAnchorClient.java
@@ -0,0 +1,10 @@
+package de.vatinmc.cursoranchor.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CursorAnchorClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
