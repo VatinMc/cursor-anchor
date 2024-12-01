8ca58b3dcfebadf184917ac7363b625cde08aee5
vatinMC <mc@vatin.de>
2024-12-01 18:03:38 +0100

    use less resources, achieve same result

--- src/main/java/de/vatinmc/cursoranchor/mixin/GenericContainerScreenMixin.java
+++ src/main/java/de/vatinmc/cursoranchor/mixin/GenericContainerScreenMixin.java
@@ -2,7 +2,6 @@ package de.vatinmc.cursoranchor.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.screen.ingame.ScreenHandlerProvider;
@@ -13,31 +12,25 @@ import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import oshi.util.tuples.Pair;
 
 @Mixin(GenericContainerScreen.class)
 public abstract class GenericContainerScreenMixin extends HandledScreen<GenericContainerScreenHandler> implements ScreenHandlerProvider<GenericContainerScreenHandler> {
     @Unique
     private static Pair<Double, Double> lastPos = new Pair<>(-1.0,-1.0);
-    @Unique
-    private boolean firstFrame = true;
     public GenericContainerScreenMixin(GenericContainerScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
     }
 
-    @Inject(method = "render", at = @At("TAIL"))
-    private void render(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo info){
-        if(firstFrame){
-            if(!(lastPos.getA() == -1.0 && lastPos.getB() == -1.0)){
-                Window mcWindow = MinecraftClient.getInstance().getWindow();
-                GLFW.glfwSetCursorPos(mcWindow.getHandle(), lastPos.getA(), lastPos.getB());
-            }
-            firstFrame = false;
+    @Override
+    public void init(){
+        Window mcWindow = MinecraftClient.getInstance().getWindow();
+        if((lastPos.getA() > 0 && lastPos.getB() > 0)){
+            GLFW.glfwSetCursorPos(mcWindow.getHandle(), lastPos.getA(), lastPos.getB());
         }
+        super.init();
     }
+
     @Override
     public void close() {
         Mouse mouse = MinecraftClient.getInstance().mouse;
