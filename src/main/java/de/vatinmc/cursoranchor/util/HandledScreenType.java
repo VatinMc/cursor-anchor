ddf5f65c8de1bdb1ff40583c8a9516e244fecf0b
vatinMC <mc@vatin.de>
2024-12-01 22:41:44 +0100

    helper class to differenciate between interactable blocks

---
+++ src/main/java/de/vatinmc/cursoranchor/util/HandledScreenType.java
@@ -0,0 +1,59 @@
+package de.vatinmc.cursoranchor.util;
+
+import net.minecraft.screen.*;
+
+public enum HandledScreenType {
+    BEACON,
+    BREWINGSTAND,
+    CARTOGRAPHYTABLE,
+    CRAFTER,
+    CRAFTING,
+    ENCHANTMENT,
+    FORGING,
+    GENERIC3X3CONTAINER,
+    GENERICCONTAINER,
+    GRINDSTONE,
+    HOPPER,
+    HORSE,
+    LOOM,
+    MERCHANT,
+    SHULKERBOX,
+    STONECUTTER,
+    ERROR;
+    public static HandledScreenType getType(ScreenHandler handler){
+        if(handler instanceof BeaconScreenHandler)
+            return BEACON;
+        if(handler instanceof BrewingStandScreenHandler)
+            return BREWINGSTAND;
+        if(handler instanceof CartographyTableScreenHandler)
+            return CARTOGRAPHYTABLE;
+        if(handler instanceof CrafterScreenHandler)
+            return CRAFTER;
+        if(handler instanceof CraftingScreenHandler)
+            return CRAFTING;
+        if(handler instanceof EnchantmentScreenHandler)
+            return ENCHANTMENT;
+        if(handler instanceof ForgingScreenHandler)
+            return FORGING;
+        if(handler instanceof Generic3x3ContainerScreenHandler)
+            return GENERIC3X3CONTAINER;
+        if(handler instanceof GenericContainerScreenHandler)
+            return GENERICCONTAINER;
+        if(handler instanceof GrindstoneScreenHandler)
+            return GRINDSTONE;
+        if(handler instanceof HopperScreenHandler)
+            return HOPPER;
+        if(handler instanceof HorseScreenHandler)
+            return HORSE;
+        if(handler instanceof LoomScreenHandler)
+            return LOOM;
+        if(handler instanceof MerchantScreenHandler)
+            return MERCHANT;
+        if(handler instanceof ShulkerBoxScreenHandler)
+            return SHULKERBOX;
+        if(handler instanceof StonecutterScreenHandler)
+            return STONECUTTER;
+
+        return ERROR;
+    }
+}
