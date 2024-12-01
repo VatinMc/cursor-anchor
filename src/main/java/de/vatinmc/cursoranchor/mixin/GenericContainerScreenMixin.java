fe71222eedd179230bfabcc4e8b00c7e7549cf1b
vatinMC <mc@vatin.de>
2024-12-01 20:18:25 +0100

    use less resources, achieve same result

--- src/main/java/de/vatinmc/cursoranchor/mixin/GenericContainerScreenMixin.java
+++ src/main/java/de/vatinmc/cursoranchor/mixin/GenericContainerScreenMixin.java
@@ -1,7 +1,7 @@
 package de.vatinmc.cursoranchor.mixin;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.Mouse;
 import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.screen.ingame.ScreenHandlerProvider;
@@ -12,33 +12,30 @@ import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import oshi.util.tuples.Pair;
 
 @Mixin(GenericContainerScreen.class)
 public abstract class GenericContainerScreenMixin extends HandledScreen<GenericContainerScreenHandler> implements ScreenHandlerProvider<GenericContainerScreenHandler> {
     @Unique
-    private static Pair<Integer, Integer> lastPos = new Pair<>(0,0);
-    @Unique
-    private boolean firstFrame = true;
+    private static Pair<Double, Double> lastPos = new Pair<>(-1.0,-1.0);
     public GenericContainerScreenMixin(GenericContainerScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
     }
 
-    @Inject(method = "render", at = @At("TAIL"))
-    private void render(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo info){
-        if(firstFrame){
-            if(!(lastPos.getA() == 0 && lastPos.getB() == 0)){
-                Window testWindow = MinecraftClient.getInstance().getWindow();
-                double scale = testWindow.getScaleFactor();
-                double relativeX = lastPos.getA() * scale;
-                double relativeY = lastPos.getB() * scale;
-                GLFW.glfwSetCursorPos(testWindow.getHandle(), relativeX, relativeY);
-            }
-            firstFrame = false;
+    @Override
+    public void init(){
+        Window mcWindow = MinecraftClient.getInstance().getWindow();
+        if((lastPos.getA() > 0 && lastPos.getB() > 0)){
+            GLFW.glfwSetCursorPos(mcWindow.getHandle(), lastPos.getA(), lastPos.getB());
         }
-        lastPos = new Pair<>(mouseX, mouseY);
+        super.init();
+    }
+
+    @Override
+    public void close() {
+        Mouse mouse = MinecraftClient.getInstance().mouse;
+        lastPos = new Pair<>(mouse.getX(), mouse.getY());
+
+        super.close();
     }
 }
