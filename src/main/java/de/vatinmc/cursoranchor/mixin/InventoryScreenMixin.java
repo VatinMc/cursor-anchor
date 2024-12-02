25102282534fbf8451d0fde827eac1d9642aba8b
vatinMC <mc@vatin.de>
2024-12-02 19:09:26 +0100

    mc-version 1.19.2 - imports - render

--- src/main/java/de/vatinmc/cursoranchor/mixin/InventoryScreenMixin.java
+++ src/main/java/de/vatinmc/cursoranchor/mixin/InventoryScreenMixin.java
@@ -1,10 +1,10 @@
 package de.vatinmc.cursoranchor.mixin;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.gui.screen.recipebook.RecipeBookProvider;
 import net.minecraft.client.util.Window;
+import net.minecraft.client.util.math.MatrixStack;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -30,7 +30,7 @@ public abstract class InventoryScreenMixin implements RecipeBookProvider{
     }
 
     @Inject(method = "render", at = @At("TAIL"))
-    private void render(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo info){
+    private void render(MatrixStack matrixStack, int mouseX, int mouseY, float delta, CallbackInfo info){
         lastPos = new Pair<>(mouseX, mouseY);
     }
 }
