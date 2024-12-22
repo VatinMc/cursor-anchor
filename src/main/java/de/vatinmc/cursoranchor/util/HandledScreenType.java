a0f274c5a4a4db2b9f65cfed25cca334d3f89df0
vatinMC <mc@vatin.de>
2024-12-22 23:28:52 +0100

    deliver itemstack for config screen

--- src/main/java/de/vatinmc/cursoranchor/util/HandledScreenType.java
+++ src/main/java/de/vatinmc/cursoranchor/util/HandledScreenType.java
@@ -1,5 +1,8 @@
 package de.vatinmc.cursoranchor.util;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.screen.*;
 
 public enum HandledScreenType {
@@ -55,5 +58,29 @@ public enum HandledScreenType {
             return STONECUTTER;
 
         return ERROR;
+    }
+
+    public static ItemStack getItemStack(HandledScreenType screenType){
+        Item item = Items.BEDROCK;
+        switch (screenType){
+            case BEACON -> item = Items.BEACON;
+            case BREWINGSTAND -> item = Items.BREWING_STAND;
+            case CARTOGRAPHYTABLE -> item = Items.CARTOGRAPHY_TABLE;
+            case CRAFTER -> item = Items.CRAFTER;
+            case CRAFTING -> item = Items.CRAFTING_TABLE;
+            case ENCHANTMENT -> item = Items.ENCHANTING_TABLE;
+            case FORGING -> item = Items.ANVIL;//+ Smithingtable
+            case GENERIC3X3CONTAINER -> item = Items.DROPPER;//+ Dispencer
+            case GENERICCONTAINER -> item = Items.CHEST;//+ Redstonechest, Barrel, Enderchest
+            case GRINDSTONE ->  item = Items.GRINDSTONE;
+            case HOPPER -> item = Items.HOPPER;
+            case HORSE -> item = Items.GOLDEN_HORSE_ARMOR;
+            case LOOM -> item = Items.LOOM;
+            case MERCHANT -> item = Items.EMERALD;
+            case SHULKERBOX -> item = Items.SHULKER_BOX;
+            case STONECUTTER -> item = Items.STONECUTTER;
+        }
+
+        return new ItemStack(item, 1);
     }
 }
